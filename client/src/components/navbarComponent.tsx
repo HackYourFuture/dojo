4285333d78b2dac5a22da6c1c02630240a5b7a58
Aya-Alabrash <aya026688@gmail.com>
2024-02-08 21:36:52 +0100

    Add HYF logo and change navbar colors.

--- client/src/components/navbarComponent.tsx
+++ client/src/components/navbarComponent.tsx
@@ -1,29 +1,18 @@
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
-import MenuIcon from '@mui/icons-material/Menu';
-import IconButton from '@mui/material/IconButton';
 import Stack from '@mui/material/Stack';
+import HYFLogo from '../assets/HYF.svg';
 
 function NavbarComponent() {
   return (
     <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static">
+      <AppBar position="static" sx={{ bgcolor: "black" }}>
         <Toolbar>
-          <IconButton
-            size="large"
-            edge="start"
-            color="inherit"
-            aria-label="menu"
-            sx={{ mr: 2 }}
-          >
-            <MenuIcon />
-          </IconButton>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            Dojo menu
-          </Typography>
+          <Box component="div" sx={{ flexGrow: 1 }}>
+            <img src={HYFLogo} height='60' alt="HYF logo"/>
+          </Box>
           <Stack direction= "row" spacing={2}>
             <Button href={`/search`} color="inherit">Search</Button>
             <Button href={`/login`} color="inherit">Login</Button>
