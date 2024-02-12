c351c07da9457e12e0437e7c3cb50550ea6c0b06
stasel <2033301+stasel@users.noreply.github.com>
2024-02-12 21:44:40 +0100

    Added backend models and dummmy data for search and trainee endpoints

--- server/src/controllers/TraineeController.ts
+++ server/src/controllers/TraineeController.ts
@@ -1,4 +1,5 @@
 import { Request, Response } from "express";
+import { Trainee } from "../models/Trainee";
 
 export interface TraineeControllerType {
   getTrainee(req: Request, res: Response): void;
@@ -11,7 +12,7 @@ export class TraineeController {
   constructor() {}
 
   getTrainee(req: Request, res: Response) {
-    res.status(500).send("Not implemented");
+    res.status(200).json(fakeTrainee);
   }
 
   createTrainee(req: Request, res: Response) {
@@ -25,4 +26,31 @@ export class TraineeController {
   deleteTrainee(req: Request, res: Response) {
     res.status(500).send("Not implemented");
   }
+}
+
+const fakeTrainee: Trainee = {
+  id: "hy0eLu",
+  personalInfo: {
+    firstName: "Emma",
+    lastName: "Smith",
+    gender: "female"
+  },
+  contactInfo: {
+    email: "emma@gmail.com",
+    slack: "emma.sm",
+    github: "emma-dev",
+  },
+  educationInfo: {
+    startCohort: 48,
+    currentCohort: 48,
+    learningStatus: "studying",
+    strikes: [],
+    assignments: [],
+    tests: [],
+  },
+  employmentInfo: {
+    status: "Still studying",
+    employmentHistory: [],
+  },
+  interactions: [],
 }
