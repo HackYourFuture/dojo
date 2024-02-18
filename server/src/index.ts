9a1cfcc83332a6cc5a0955b2a6202912a894e2f3
stasel <2033301+stasel@users.noreply.github.com>
2024-02-19 00:22:52 +0100

    Refactored index.ts to have it's initialization in steps

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,4 +1,4 @@
-import express, { Express, Request, Response, Application } from "express";
+import express, { Request, Response, NextFunction} from "express";
 import dotenv from "dotenv";
 import cors from "cors";
 import swagger from "./api-docs/swagger";
@@ -7,38 +7,68 @@ import SearchRouter from "./routes/SearchRouter";
 import { TraineeController } from "./controllers/TraineeController";
 import { SearchController } from "./controllers/SearchController";
 
-dotenv.config();
+class Main {
+  private app: express.Application;
+  constructor() {
+    this.app = express();
+  }
 
-const app: Application = express();
-const port = process.env.PORT || 7777;
+  setupMiddlewares() {
+    if (process.env.NODE_ENV !== "production") {
+      this.app.use(cors());
+    }
+    this.app.use("/api-docs", swagger("./api.yaml"));
+    this.app.use(express.json());
+    this.app.disable("x-powered-by");
+  };
 
-// Setup middlewares
-if (process.env.NODE_ENV !== "production") {
-  app.use(cors());
+  setupRoutes() {
+    // setup controllers
+    const traineeController = new TraineeController();
+    const searchController = new SearchController();
+  
+    // Setup routers
+    const traineeRouter = new TraineeRouter(traineeController);
+    const searchRouter = new SearchRouter(searchController);
+  
+    // Define routes
+    this.app.use("/api/trainees", traineeRouter.build());
+    this.app.use("/api/search", searchRouter.build());
+  
+    // Not found handler
+    this.app.use((req: Request, res: Response) => {
+      res.status(404).json({ error: "Not found" });
+    });
+  
+    // Global error handler
+    this.app.use((error: Error, req: Request, res: Response, next: NextFunction) => {
+      console.error(error);
+      res.status(500).json({ error: 'Something broke!' });
+    });
+  };
+
+  startServer(port: number) {
+    this.app.listen(port, () => {
+      console.log(`🟢 Dojo Server is running`);
+      console.log(`    🌐 Base URL: http://localhost:${port}/api`);
+      console.log(`    📝 API docs: http://localhost:${port}/api-docs`);
+      console.log('');
+    });
+  };
+
+  async run() {
+    console.log("--------------------------------------------------------------------------------");
+    dotenv.config();
+  
+    console.log("Configuring application...");
+    this.setupMiddlewares();
+    this.setupRoutes();
+  
+    console.log("Starting Dojo Server...");
+    const port = Number.parseInt(process.env.PORT ?? "7777");
+    this.startServer(port);
+  }
 }
-app.use("/api-docs", swagger("./api.yaml"));
-app.use(express.json());
-app.disable('x-powered-by')
 
-// setup controllers
-const traineeController = new TraineeController();
-const searchController = new SearchController();
-
-// Setup routes
-const traineeRouter = new TraineeRouter(traineeController);
-const searchRouter = new SearchRouter(searchController);
-app.use("/api/trainee", traineeRouter.build());
-app.use("/api/search", searchRouter.build());
-
-// Not found handler
-app.use((req, res, next) => {
-    res.status(404).json({ error: 'Not found' });
-});
-
-// Start application
-app.listen(port, () => {
-  console.log(`🟢 Dojo Server is running`);
-  console.log(`    🌐 Base URL: http://localhost:${port}/api`);
-  console.log(`    📝 API docs: http://localhost:${port}/api-docs`);
-
-});
+const main = new Main();
+main.run().then().catch(console.error);
