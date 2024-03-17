ac8d8c60d8e62fc522d4369e5011a93b00dfbd7e
stasel <2033301+stasel@users.noreply.github.com>
2024-03-17 21:53:25 +0100

    Enabled Vite proxy server to route all /apis request directly to the backend api. This will prevent sending CORS requests

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -4,4 +4,14 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    // Automatically open the app in the browser on server start.
+    open: '/',
+
+    // Proxy /api requests to the API server. This will avoid any CORS issues.
+    proxy: {
+      '/api': 'http://localhost:7777',
+      '/api-docs': 'http://localhost:7777',
+    },
+  },
 })
