ce2b857e7858ae75c39a2791a23ba058b36f3b9d
stasel <2033301+stasel@users.noreply.github.com>
2024-03-18 13:19:20 +0100

    Renamed env variable

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -4,7 +4,8 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig(({ command, mode }) => {
   const env = loadEnv(mode, process.cwd(), 'VITE_');
-  const apiUrl = env.VITE_API_URL ?? "http://localhost:7777";
+  console.log(env);
+  const backendProxyTarget = env.VITE_BACKEND_PROXY_TARGET ?? "http://localhost:7777";
   return {
     plugins: [react()],
     server: {
@@ -13,8 +14,8 @@ export default defineConfig(({ command, mode }) => {
 
       // Proxy /api requests to the API server. This will avoid any CORS issues.
       proxy: {
-        "/api": apiUrl,
-        "/api-docs": apiUrl,
+        "/api": backendProxyTarget,
+        "/api-docs": backendProxyTarget,
       },
     },
   };
