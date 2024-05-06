dd2e0502b02f7073c181aeb6a5c37f77a35a4480
AYA AL-ABRASH <35225101+Aya-Alabrash@users.noreply.github.com>
2024-05-06 20:58:53 +0200

    improve update/save data functionality.

--- client/src/components/ErrorBox.tsx
+++ client/src/components/ErrorBox.tsx
@@ -1,8 +1,5 @@
 import { Alert } from "@mui/material";
-
-interface ErrorBoxProps {
-  errorMessage: string;
-}
+import { ErrorBoxProps } from "../types";
 
 export const ErrorBox = (props: ErrorBoxProps) => {
   return (
--- client/src/components/TraineeProfilePage.tsx
+++ client/src/components/TraineeProfilePage.tsx
@@ -7,13 +7,43 @@ import {
   EducationInfo,
   EmploymentInfo,
 } from "./index";
-import { Box } from "@mui/material";
+import { Alert, AlertTitle, Box } from "@mui/material";
+import { Loader } from "./Loader";
+import { useParams } from "react-router-dom";
+import { useTraineeInfoData } from "../hooks/useTraineeInfoData";
+import { TraineeInfo } from "../types";
 
 export const TraineeProfilePage = () => {
-  const [activeTab, setActiveTab] = useState("personal"); // Default active tab
+  // Default active tab
+  const [activeTab, setActiveTab] = useState("personal");
+
+  const { traineeInfo } = useParams();
+  const trainee = traineeInfo?.split("-");
+  const traineeId = trainee ? trainee[1] : "";
+  const { isLoading, isError, data, error, isFetching } =
+    useTraineeInfoData(traineeId);
+
+  if (isLoading || isFetching) {
+    return <Loader />;
+  }
+
+  if (isError && error instanceof Error) {
+    return (
+      <Box p={8} sx={{ width: "100%" }}>
+        <Alert severity="error">
+          <AlertTitle>Error</AlertTitle>
+          {error.message}
+        </Alert>
+      </Box>
+    );
+  }
 
   const handleTabChange = (tab: string) => {
     setActiveTab(tab);
+  };
+
+  const saveTraineeData = (editedData: Partial<TraineeInfo>) => {
+    console.log("Save edited data:", editedData);
   };
 
   return (
@@ -31,8 +61,18 @@ export const TraineeProfilePage = () => {
       </Box>
       <Box width="100%" paddingY="16px">
         <ProfileNav activeTab={activeTab} onTabChange={handleTabChange} />
-        {activeTab === "personal" && <PersonalInfo />}
-        {activeTab === "contact" && <ContactInfo />}
+        {activeTab === "personal" && (
+          <PersonalInfo
+            traineeData={data && data.personalInfo}
+            saveTraineeData={saveTraineeData}
+          />
+        )}
+        {activeTab === "contact" && (
+          <ContactInfo
+            contactData={data && data.contactInfo}
+            saveTraineeData={saveTraineeData}
+          />
+        )}
         {activeTab === "education" && <EducationInfo />}
         {activeTab === "employment" && <EmploymentInfo />}
       </Box>
