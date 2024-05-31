fbfdad4594adb96e3bd88e7879ad910d2282adfe
simaa_backbase <simaa@backbase.com>
2024-05-31 14:07:45 +0200

    add missing enums and t and types

--- client/src/types.ts
+++ client/src/types.ts
@@ -1,3 +1,28 @@
+// enums
+export enum LearningStatus {
+  Studying = "studying",
+  Graduated = "graduated",
+  OnHold = "on-hold",
+  Quit = "quit",
+}
+
+export enum JobPath {
+  NotGraduated = "not-graduated",
+  Searching = "searching",
+  Internship = "internship",
+  TechJob = "tech-job",
+  NonTechJob = "non-tech-job",
+  NotSearching = "not-searching",
+  OtherStudies = "other-studies",
+  NoLongerHelping = "no-longer-helping",
+}
+
+export enum EmploymentType {
+  Internship = "internship",
+  Job = "job",
+}
+
+// interfaces
 export interface SearchResult {
   id: number;
   name: string;
@@ -14,9 +39,14 @@ export interface ProfileNavProps {
 
 export interface TraineeInfo {
   id: string;
+  readonly createdAt: Date;
+  readonly updatedAt: Date;
+  displayName: string;
   personalInfo: TraineeData;
   contactInfo: ContactData;
-
+  educationInfo: EducationData;
+  employmentInfo: EmploymentData;
+  interactions: InteractionData[];
 }
 
 export interface TraineeData {
@@ -48,3 +78,76 @@ export interface ContactData {
   githubHandle: string;
   linkedin: string;
 }
+
+
+export interface EducationData {
+  startCohort: number;
+  currentCohort?: number;
+  learningStatus: LearningStatus;
+  startDate?: Date;
+  graduationDate?: Date;
+  // use enum for quitReason when needed
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  quitReason?: any;
+  quitDate?: Date;
+  strikes: Strike[];
+  assignments: Assignment[];
+  // use enum for tests when needed
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  tests: any;
+  comments?: string;
+}
+
+export interface EmploymentData {
+  jobPath: JobPath;
+  cvURL?: string;
+  availability?: string;
+  preferredRole?: string;
+  drivingLicense?: boolean;
+  preferredLocation?: string;
+  extraTechnologies?: string;
+  employmentHistory: EmploymentHistory[];
+  comments?: string;
+}
+
+export interface InteractionData {
+  readonly _id: string;
+  date: Date;
+  // use enum for type when needed
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  type: any;
+  reporterID: string;
+  details: string;
+}
+
+export interface EmploymentHistory {
+  readonly _id: string;
+  type: EmploymentType;
+  companyName: string;
+  role: string;
+  startDate: Date;
+  endDate?: Date;
+  feeCollected: boolean;
+  feeAmount?: number;
+  comments?: string;
+}
+
+export interface Strike {
+  readonly _id: string;
+  date: Date;
+  reporterID: string;
+  // use enum for reason when needed
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  reason: any;
+  comments: string;
+}
+
+export interface Assignment {
+  readonly _id: string;
+  createDate: Date;
+  type: string;
+  status: string;
+  content?: string;
+  comments?: string;
+}
+
