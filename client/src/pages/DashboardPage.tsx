65adfecdbeea391db793937d61d4fdbaef26134c
simaa_backbase <simaa@backbase.com>
2024-06-18 12:29:35 +0200

    create dashboard page

--- client/src/pages/DashboardPage.tsx
+++ client/src/pages/DashboardPage.tsx
@@ -1,3 +1,55 @@
+import { useState } from 'react';
+import { useDashboardData } from '../hooks/useDashboardData';
+import { DashboardPieChart, ErrorBox, Loader } from '../components';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { DatePicker } from '@mui/x-date-pickers/DatePicker';
+import dayjs, { Dayjs } from 'dayjs';
+import { Container, Stack, Box, Button } from '@mui/material';
+
 export const DashboardPage = () => {
-  return <div>Dashboard Page</div>;
+  const today = new Date();
+  const [startDate, setStartDate] = useState<Dayjs | null>(dayjs(today));
+  const [endDate, setEndDate] = useState<Dayjs | null>(dayjs(today));
+
+  const startDateFormatted: string | undefined = startDate?.format('YYYY-MM-DD');
+  const endDateFormatted: string | undefined = endDate?.format('YYYY-MM-DD');
+
+  const { isLoading, isError, data, error, isFetching, refetch } = useDashboardData(
+    startDateFormatted,
+    endDateFormatted
+  );
+
+  if (isLoading || isFetching) {
+    return <Loader />;
+  }
+
+  if (isError && error instanceof Error) {
+    return <ErrorBox errorMessage={error.message} />;
+  }
+
+  return (
+    <Container fixed>
+      <Box my={3} display="flex" alignItems="start" justifyContent="start" p={2}>
+        <Stack direction="row" spacing={3}>
+          <LocalizationProvider dateAdapter={AdapterDayjs}>
+            <DatePicker
+              label="Start date"
+              value={startDate}
+              onChange={(newValue) => setStartDate(newValue)}
+            />
+            <DatePicker
+              label="End date"
+              value={endDate}
+              onChange={(newValue) => setEndDate(newValue)}
+            />
+          </LocalizationProvider>
+          <Button variant="contained" onClick={() => refetch()}>
+            Update
+          </Button>
+        </Stack>
+      </Box>
+      {data && <DashboardPieChart chartData={data}></DashboardPieChart>}
+    </Container>
+  );
 };
