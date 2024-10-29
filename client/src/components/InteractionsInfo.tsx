12bdcd84f28bf40c2d9bc66e86fce9dcc4487253
hhana1-1 <hhana1@live.com>
2024-10-29 11:06:02 +0100

    removed commented out code

--- client/src/components/InteractionsInfo.tsx
+++ client/src/components/InteractionsInfo.tsx
@@ -1,8 +1,3 @@
-// import { useEffect, useState } from 'react';
-// import { Box, Button, FormControl, Icon, InputAdornment, Link, Stack, TextField } from '@mui/material';
-
-// import { TraineeContactInfo } from '../models';
-
 export const InteractionsInfo = () => {
-  return <p style={{ color: 'black', padding: '24px' }}>This is the Interactions tab.</p>;
-};
+    return <p style={{ color: 'black', padding: '24px' }}>This is the Interactions tab.</p>;
+  };
