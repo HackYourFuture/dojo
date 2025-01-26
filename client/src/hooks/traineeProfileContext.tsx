fab81923ffdb1047b50c2b2ec0acb220d0d28647
zalexa19 <zalexa19@gmail.com>
2025-01-26 16:51:27 +0100

    added context for traineeId

---
+++ client/src/hooks/traineeProfileContext.tsx
@@ -0,0 +1,21 @@
+import React, { createContext, useContext, useState } from 'react';
+
+type TraineeProfileContextType = {
+  traineeId: string | null;
+  setTraineeId: (id: string | null) => void;
+};
+
+const TraineeProfileContext = createContext<TraineeProfileContextType>({
+  traineeId: null,
+  setTraineeId: (id) => {},
+});
+
+export const TraineeProfileProvider = ({ id, children }: { id: string; children: React.ReactNode }) => {
+  const [traineeId, setTraineeId] = useState<string | null>(id);
+
+  return (
+    <TraineeProfileContext.Provider value={{ traineeId, setTraineeId }}>{children}</TraineeProfileContext.Provider>
+  );
+};
+
+export const useTraineeProfileContext = () => useContext(TraineeProfileContext);
