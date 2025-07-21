f888d9ef636d27c7afa422fa377864cc5a13105e
stasel <2033301+stasel@users.noreply.github.com>
2025-07-21 22:55:44 +0200

    Increasted minimum JWT token length from 16 to 32

--- server/src/services/TokenService.ts
+++ server/src/services/TokenService.ts
@@ -12,8 +12,8 @@ export class TokenService implements TokenServiceType {
   private readonly ALGORITHM = 'HS512';
 
   constructor(secret: string, expirationInDays: number) {
-    if (!secret || secret.length < 16) {
-      throw new Error('JWT Secret is required');
+    if (!secret || secret.length < 32) {
+      throw new Error('JWT Secret must be at least 32 characters long for security');
     }
     this.secret = secret;
     this.expiration = `${expirationInDays}d`;
