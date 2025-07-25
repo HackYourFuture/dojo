3230bab9de46812461677710932398bdb940f356
Muatasim-Aswad <malasoad2021@gmail.com>
2025-07-25 23:16:39 +0200

    feat: add user CRUD methods to the repo

--- server/src/repositories/UserRepository.ts
+++ server/src/repositories/UserRepository.ts
@@ -1,10 +1,16 @@
 import mongoose from 'mongoose';
 import { UserSchema } from '../schemas';
-import { User } from '../models';
+
+import type { CreateUser, UpdateUser, User } from '../models';
 
 export interface UserRepository {
   findUserByID(id: string): Promise<User | null>;
   findUserByEmail(email: string): Promise<User | null>;
+
+  getAllUsers(): Promise<User[]>;
+  createUser(user: CreateUser): Promise<User>;
+  updateUser(id: string, user: Partial<UpdateUser>): Promise<User | null>;
+  deleteUser(id: string): Promise<User | null>;
 }
 
 export class MongooseUserRepository implements UserRepository {
@@ -21,4 +27,12 @@ export class MongooseUserRepository implements UserRepository {
   async findUserByEmail(email: string): Promise<User | null> {
     return await this.UserModel.findOne({ email });
   }
+
+  getAllUsers: UserRepository['getAllUsers'] = () => this.UserModel.find();
+
+  createUser: UserRepository['createUser'] = (user: User) => this.UserModel.create(user);
+
+  updateUser: UserRepository['updateUser'] = (id, user) => this.UserModel.findByIdAndUpdate(id, user, { new: true });
+
+  deleteUser: UserRepository['deleteUser'] = (id) => this.UserModel.findByIdAndDelete(id);
 }
