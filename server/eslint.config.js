d5e3b670019a6e8915e6893c2bd4641dbd165cc2
muatAsim <167855567+Muatasim-Aswad@users.noreply.github.com>
2025-07-28 09:56:54 +0200

    chore: setup TypeScript linting with ESLint (#244)

---
+++ server/eslint.config.js
@@ -0,0 +1,25 @@
+// @ts-check
+
+import eslint from '@eslint/js';
+import tseslint from 'typescript-eslint';
+
+export default tseslint.config(
+  eslint.configs.recommended,
+  tseslint.configs.strictTypeChecked,
+  tseslint.configs.stylisticTypeChecked,
+  {
+    languageOptions: {
+      parserOptions: {
+        project: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+  },
+  {
+    rules: {
+      '@typescript-eslint/restrict-template-expressions': ['error', { allowNumber: true }],
+      'no-unused-vars': 'off',
+      '@typescript-eslint/no-unused-vars': 'error',
+    },
+  }
+);
