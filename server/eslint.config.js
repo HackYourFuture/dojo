db3c8b123987cbc897f7693b26f1436a2cfdbfe7
Muatasim-Aswad <malasoad2021@gmail.com>
2025-07-29 10:23:48 +0200

    update client linter

--- server/eslint.config.js
+++ server/eslint.config.js
@@ -5,7 +5,7 @@ import tseslint from 'typescript-eslint';
 
 export default tseslint.config(
   {
-    ignores: ['dist/**', 'node_modules/**', '**/*.js'],
+    ignores: ['dist', 'node_modules'],
   },
   eslint.configs.recommended,
   tseslint.configs.strictTypeChecked,
