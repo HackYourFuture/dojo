a00b58f894f5a3f39857c6eeb23c74bc0ae0453d
Abdul Aziz Hatkou <zizochampion@hotmail.com>
2025-10-20 10:29:47 +0000

    Added TestList.tsx -shows all test entries of a student -result icon depend on the grade result -does not fully implement the edit and delete buttons yet

---
+++ client/src/components/education/TestsList.tsx
@@ -0,0 +1,109 @@
+import { Box, Icon, IconButton, List, ListItem, ListItemText, Tooltip, Typography } from '@mui/material';
+import EditIcon from '@mui/icons-material/Edit';
+import DeleteIcon from '@mui/icons-material/Delete';
+import { Test, TestResult } from '../../models';
+import { CheckCircle, OfflinePin, CancelSharp, Error } from '@mui/icons-material';
+import { formatDateForDisplay } from '../../helpers/dateHelper';
+import MarkdownText from '../shared/MarkdownText';
+
+interface TestsListProps {
+  tests: Test[];
+  onClickEdit: (id: string) => void;
+  onClickDelete: (id: string) => void;
+}
+
+export const TestsList: React.FC<TestsListProps> = ({ tests, onClickEdit, onClickDelete }) => {
+  const formatTestComment = (comment: string): string => {
+    return comment.split('-').join(' ').charAt(0).toUpperCase() + comment.split('-').join(' ').slice(1).toLowerCase();
+  };
+
+  const handleEdit = (id: string) => {
+    onClickEdit(id);
+  };
+  const handleDelete = (id: string) => {
+    onClickDelete(id);
+  };
+
+  const resultIconMap = (testResult: TestResult) => {
+    switch (testResult) {
+      case TestResult.Passed:
+        return <CheckCircle color="success" />;
+      case TestResult.PassedWithWarning:
+        return <OfflinePin color="warning" />;
+      case TestResult.Failed:
+        return <CancelSharp color="error" />;
+      case TestResult.Disqualified:
+        return <Error color="error" />;
+      default:
+        return <Icon>help_outline</Icon>;
+    }
+  };
+
+  const renderActions = (id: string) => {
+    return (
+      <Box sx={{ display: 'flex', alignItems: 'center', justifyContent: 'flex-end', paddingRight: 1 }}>
+        <IconButton aria-label="edit" onClick={() => handleEdit(id)}>
+          <EditIcon />
+        </IconButton>
+        <IconButton aria-label="delete" onClick={() => handleDelete(id)}>
+          <DeleteIcon />
+        </IconButton>
+      </Box>
+    );
+  };
+
+  return (
+    <List
+      sx={{
+        width: '100%',
+        bgcolor: 'background.paper',
+        maxHeight: 300,
+        overflow: 'auto',
+        scrollbarWidth: 'thin',
+      }}
+    >
+      {tests.length === 0 ? (
+        <Typography variant="body1" color="#CCCCCC" padding="16px">
+          No tests found
+        </Typography>
+      ) : (
+        tests.map((test: Test, index: number) => {
+          return (
+            <Box key={test.id}>
+              <ListItem
+                alignItems="flex-start"
+                disablePadding
+                sx={{
+                  paddingBottom: 1,
+                  bgcolor: index % 2 === 0 ? '#f8f9fa' : 'background.paper',
+                }}
+              >
+                <ListItemText
+                  primary={
+                    <Box
+                      display="flex"
+                      flexDirection="row"
+                      justifyContent="space-between"
+                      width="100%"
+                      paddingTop={1}
+                      paddingBottom={1}
+                    >
+                      <Box display="flex">
+                        <Tooltip title={test.result}>{resultIconMap(test.result)}</Tooltip>
+                        {formatTestComment(test.type || '')}
+                      </Box>
+                      <Typography sx={{ paddingRight: 2 }}>{test.score ?? 'N/A'}</Typography>
+                      <Typography sx={{ paddingRight: 2 }}>{formatDateForDisplay(test.date)}</Typography>
+                    </Box>
+                  }
+                  secondary={<MarkdownText>{test.comments ?? ''}</MarkdownText>}
+                />
+                {renderActions(test.id)}
+              </ListItem>
+            </Box>
+          );
+        })
+      )}
+    </List>
+  );
+};
