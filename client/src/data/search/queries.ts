b2640a980b152ef8c115f865f86f4731fbe52c18
zalexa19 <zalexa19@gmail.com>
2026-01-31 20:47:01 +0100

    fixed according to comments

--- client/src/data/search/queries.ts
+++ client/src/data/search/queries.ts
@@ -12,7 +12,6 @@ export const useSearch = (query: string) => {
   return useQuery({
     queryKey: searchKeys.byQuery(query),
     queryFn: async () => {
-      // TODO: extract URL to an api.ts
       const response = await axios.get(`/api/search?q=${query}&limit=20`);
       const trainees: SearchResult[] = response.data.hits.data.map((trainee: SearchResult) => trainee);
       return trainees;
@@ -21,4 +20,14 @@ export const useSearch = (query: string) => {
     refetchOnMount: false,
     refetchOnWindowFocus: false,
   });
+};
+
+interface SearchResultResponse {
+  hits: {
+    data: SearchResult[];
+  };
+}
+export const getSearchResults = async (query: string): Promise<SearchResult[]> => {
+  const response = await axios.get<SearchResultResponse>(`/api/search?q=${query}&limit=20`);
+  return response.data.hits.data;
 };
