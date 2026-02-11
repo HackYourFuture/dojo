491fad52cfe49dce261d294e5ecacbde9bc38c9b
zalexa19 <zalexa19@gmail.com>
2026-02-11 22:11:49 +0100

    Fixed typo in non-tech

--- client/src/features/trainee-profile/profile/components/JobPathSelect.tsx
+++ client/src/features/trainee-profile/profile/components/JobPathSelect.tsx
@@ -1,10 +1,11 @@
+import { formatJobPathToLabel, formatTextToFriendly } from '../../utils/formHelper';
+
 import { DropdownSelect } from './DropdownSelect';
 import { JobPath } from '../../../../data/types/Trainee';
 import { SelectChangeEvent } from '@mui/material';
-import { formatTextToFriendly } from '../../utils/formHelper';
 
 const options = Object.values(JobPath).map((status) => ({
-  label: formatTextToFriendly(status),
+  label: formatJobPathToLabel(status),
   value: status,
 }));
 
