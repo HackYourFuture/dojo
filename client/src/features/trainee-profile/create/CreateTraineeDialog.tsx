e776fdc16651ffa97d73cb85be92c77eae296003
zalexa19 <zalexa19@gmail.com>
2026-02-11 08:52:16 +0100

    wip: moved the form to its own ncomponent

--- client/src/features/trainee-profile/create/CreateTraineeDialog.tsx
+++ client/src/features/trainee-profile/create/CreateTraineeDialog.tsx
@@ -1,14 +1,57 @@
-import { Background, Gender, JobPath, LearningStatus } from '../../../data/types/Trainee';
-import { Box, Button, Dialog, SelectChangeEvent, Stack, Typography } from '@mui/material';
+import { Box, Dialog, SelectChangeEvent, Typography } from '@mui/material';
+import { FormErrors, FormState, NewTraineeForm } from './components/NewTraineeForm';
+import { JobPath, LearningStatus } from '../../../data/types/Trainee';
+import { SubmitEventHandler, useState } from 'react';
 
-import { DropdownSelect } from '../profile/components/DropdownSelect';
-import { GenderSelect } from '../profile/components/GenderSelect';
-import TextFieldWrapper from './components/TextFieldWrapper';
-import { useCreateTraineeForm } from './hooks/useCreateTraineeForm';
+import { useCreateTraineeProfile } from './data/mutations';
+import { validateForm } from './lib/formHelper';
 
 export const CreateTraineeDialog: React.FC = () => {
-  const { formState, onSelectGender, handleChange, handleSelect, handleSubmit, errors, isPending } =
-    useCreateTraineeForm();
+  const { mutate: createTrainee, isPending, data } = useCreateTraineeProfile();
+  const [errors, setErrors] = useState<FormErrors>({});
+
+  const [formState, setFormState] = useState<FormState>({
+    firstName: '',
+    lastName: '',
+    gender: null,
+    email: '',
+    cohort: 0,
+    learningStatus: LearningStatus.Studying,
+    jobPath: JobPath.NotGraduated,
+  });
+
+  const handleTextChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setFormState((prevState: FormState) => ({
+      ...prevState,
+      [name]: value,
+    }));
+  };
+
+  const handleSelectChange = (event: SelectChangeEvent<String | number>) => {
+    const { name, value } = event.target;
+
+    setFormState((prevState: FormState) => ({
+      ...prevState,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit: SubmitEventHandler<HTMLFormElement> = (event) => {
+    event.preventDefault();
+    setErrors({});
+    const errors = validateForm(formState);
+    setErrors(errors);
+
+    const hasErrors = Object.values(errors).some((error) => error != null);
+
+    if (hasErrors) {
+      console.log('form has errors, not submitting', errors);
+      return;
+    }
+
+    createTrainee(formState);
+  };
 
   return (
     <Dialog open={true} onClose={() => {}} fullWidth maxWidth="sm">
@@ -16,93 +59,14 @@ export const CreateTraineeDialog: React.FC = () => {
         <Typography variant="h4" gutterBottom>
           New trainee profile
         </Typography>
-        {/* Form fields for creating a trainee profile would go here */}
-
-        <form onSubmit={handleSubmit}>
-          <Stack spacing={2}>
-            <TextFieldWrapper
-              disabled={isPending}
-              id="firstName"
-              name="firstName"
-              error={!!errors.firstName}
-              helperText={errors.firstName}
-              label="First name"
-              value={formState.firstName}
-              onChange={handleChange}
-            />
-            <TextFieldWrapper
-              disabled={isPending}
-              id="lastName"
-              name="lastName"
-              error={!!errors.lastName}
-              helperText={errors.lastName}
-              label="Last name"
-              value={formState.lastName}
-              onChange={handleChange}
-            />
-            <GenderSelect
-              disabled={isPending}
-              error={errors.gender || ''}
-              isEditing={true}
-              gender={formState.gender}
-              onChange={onSelectGender}
-            />
-            <TextFieldWrapper
-              disabled={isPending}
-              id="email"
-              name="email"
-              error={!!errors.email}
-              helperText={errors.email}
-              label="Email"
-              value={formState.email}
-              onChange={handleChange}
-            />
-            <TextFieldWrapper
-              disabled={isPending}
-              id="cohort"
-              name="cohort"
-              type="number"
-              error={!!errors.cohort}
-              helperText={errors.cohort}
-              label="Start cohort"
-              value={formState.cohort}
-              onChange={handleChange}
-              maxLength={3}
-            />
-            <DropdownSelect
-              disabled={isPending}
-              inputLabel="Learning status"
-              id="learningStatus"
-              name="learningStatus"
-              label="Learning status"
-              options={Object.values(LearningStatus)}
-              isEditing
-              value={formState.learningStatus}
-              error={errors.learningStatus || ''}
-              onChange={handleSelect}
-            />
-            <DropdownSelect
-              disabled={isPending}
-              inputLabel="Job path"
-              id="jobPath"
-              name="jobPath"
-              label="jobPath"
-              options={Object.values(JobPath)}
-              isEditing
-              value={formState.jobPath}
-              error={errors.jobPath || ''}
-              onChange={handleSelect}
-            />
-          </Stack>
-          <Stack direction="row" spacing={2} justifyContent="flex-end" mt={2}>
-            <Button variant="outlined" color="secondary" disabled={isPending} onClick={() => {}}>
-              Cancel
-            </Button>
-            <Button type="submit" variant="contained" color="primary" loading={isPending} disabled={isPending}>
-              Create
-            </Button>
-          </Stack>
-        </form>
+        <NewTraineeForm
+          isLoading={isPending}
+          formState={formState}
+          errors={errors}
+          handleChange={handleTextChange}
+          handleSelect={handleSelectChange}
+          handleSubmit={handleSubmit}
+        />
       </Box>
     </Dialog>
   );
---
+++ client/src/features/trainee-profile/create/components/NewTraineeForm.tsx
@@ -0,0 +1,122 @@
+import { Button, SelectChangeEvent, Stack } from '@mui/material';
+import { Gender, JobPath, LearningStatus } from '../../../../data/types/Trainee';
+
+import { DropdownSelect } from '../../profile/components/DropdownSelect';
+import { SubmitEventHandler } from 'react';
+import TextFieldWrapper from './TextFieldWrapper';
+
+export type FormState = {
+  firstName: string;
+  lastName: string;
+  gender: Gender | null;
+  email: string;
+  cohort: number;
+  learningStatus: LearningStatus;
+  jobPath: JobPath;
+};
+
+export type FormErrors = {
+  [K in keyof FormState]?: string | null;
+};
+
+export const NewTraineeForm: React.FC<{
+  isLoading: boolean;
+  formState: FormState;
+  errors: FormErrors;
+  handleChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  handleSelect: (event: SelectChangeEvent<String | number>) => void;
+  handleSubmit: SubmitEventHandler<HTMLFormElement>;
+}> = ({ isLoading, formState, errors, handleChange, handleSelect, handleSubmit }) => {
+  return (
+    <form onSubmit={handleSubmit}>
+      <Stack spacing={2}>
+        <TextFieldWrapper
+          disabled={isLoading}
+          id="firstName"
+          name="firstName"
+          error={!!errors.firstName}
+          helperText={errors.firstName}
+          label="First name"
+          value={formState.firstName}
+          onChange={handleChange}
+        />
+        <TextFieldWrapper
+          disabled={isLoading}
+          id="lastName"
+          name="lastName"
+          error={!!errors.lastName}
+          helperText={errors.lastName}
+          label="Last name"
+          value={formState.lastName}
+          onChange={handleChange}
+        />
+        <DropdownSelect
+          disabled={isLoading}
+          inputLabel="Gender"
+          id="gender"
+          name="gender"
+          label="gennder"
+          options={Object.values(Gender)}
+          isEditing
+          value={formState.gender || undefined}
+          error={errors.gender || ''}
+          onChange={handleSelect}
+        />
+        <TextFieldWrapper
+          disabled={isLoading}
+          id="email"
+          name="email"
+          error={!!errors.email}
+          helperText={errors.email}
+          label="Email"
+          value={formState.email}
+          onChange={handleChange}
+        />
+        <TextFieldWrapper
+          disabled={isLoading}
+          id="cohort"
+          name="cohort"
+          type="number"
+          error={!!errors.cohort}
+          helperText={errors.cohort}
+          label="Start cohort"
+          value={formState.cohort}
+          onChange={handleChange}
+          maxLength={3}
+        />
+        <DropdownSelect
+          disabled={isLoading}
+          inputLabel="Learning status"
+          id="learningStatus"
+          name="learningStatus"
+          label="Learning status"
+          options={Object.values(LearningStatus)}
+          isEditing
+          value={formState.learningStatus}
+          error={errors.learningStatus || ''}
+          onChange={handleSelect}
+        />
+        <DropdownSelect
+          disabled={isLoading}
+          inputLabel="Job path"
+          id="jobPath"
+          name="jobPath"
+          label="jobPath"
+          options={Object.values(JobPath)}
+          isEditing
+          value={formState.jobPath}
+          error={errors.jobPath || ''}
+          onChange={handleSelect}
+        />
+      </Stack>
+      <Stack direction="row" spacing={2} justifyContent="flex-end" mt={2}>
+        <Button variant="outlined" color="secondary" disabled={isLoading} onClick={() => {}}>
+          Cancel
+        </Button>
+        <Button type="submit" variant="contained" color="primary" loading={isLoading} disabled={isLoading}>
+          Create
+        </Button>
+      </Stack>
+    </form>
+  );
+};
