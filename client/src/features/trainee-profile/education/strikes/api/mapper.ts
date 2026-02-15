52551f85096058e2aa118d13bf9725d953ddab20
zalexa19 <zalexa19@gmail.com>
2026-02-15 11:47:27 +0100

    added a mappers for domain types

---
+++ client/src/features/trainee-profile/education/strikes/api/mapper.ts
@@ -0,0 +1,41 @@
+import { Strike, StrikeReason } from '../models/strike';
+import { StrikeRequest, StrikeResponse } from './types';
+
+export const mapStrikeToDomain = (dto: StrikeResponse): Strike => {
+  return {
+    id: dto.id,
+    comments: dto.comments,
+    date: new Date(dto.date),
+    reason: mapStringToStrikeReason(dto.reason),
+    reporterName: dto.reporter.name,
+    reporterImageUrl: dto.reporter.imageUrl,
+  };
+};
+
+export const mapDomainToStrikeRequest = (strike: Strike): StrikeRequest => {
+  return {
+    reason: strike.reason,
+    date: strike.date.toISOString(),
+    comments: strike.comments,
+  };
+};
+const mapStringToStrikeReason = (reason: string): StrikeReason => {
+  switch (reason) {
+    case 'late-submission':
+      return StrikeReason.LateSubmission;
+    case 'missed-submission':
+      return StrikeReason.MissedSubmission;
+    case 'incomplete-submission':
+      return StrikeReason.IncompleteSubmission;
+    case 'late-attendance':
+      return StrikeReason.LateAttendance;
+    case 'absence':
+      return StrikeReason.Absence;
+    case 'pending-feedback':
+      return StrikeReason.PendingFeedback;
+    case 'other':
+      return StrikeReason.Other;
+    default:
+      throw new Error(`Unknown strike reason: ${reason}`);
+  }
+};
