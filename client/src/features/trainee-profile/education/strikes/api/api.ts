2f516bb40485efc525056f76c3f7a1b33f19ab6c
zalexa19 <zalexa19@gmail.com>
2026-02-15 11:40:26 +0100

    added global error handling to queries as well

--- client/src/features/trainee-profile/education/strikes/api/api.ts
+++ client/src/features/trainee-profile/education/strikes/api/api.ts
@@ -1,21 +1,13 @@
+import { Strike } from '../models/strike';
 import axios from 'axios';
-import { Strike } from '../../../../../data/types/Trainee';
 
 export const getStrikes = async (traineeId: string) => {
-  const { data } = await axios.get<Strike[]>(`/api/trainees/${traineeId}/strikes`);
+  const { data } = await axios.get(`/api/trainees/${traineeId}/strikes`);
   return data;
 };
 
 export const addStrike = async (traineeId: string, strike: Strike) => {
-  try {
-    await axios.post(`/api/trainees/${traineeId}/strikes`, strike);
-  } catch (error) {
-    if (axios.isAxiosError(error)) {
-      throw new Error(error.response?.data?.error || 'Failed to add strike');
-    }
-
-    throw new Error(error instanceof Error ? error.message : 'An unexpected error occurred');
-  }
+  await axios.post(`/api/trainees/${traineeId}/strikes`, strike);
 };
 
 export const deleteStrike = async (traineeId: string, strikeId: string) => {
@@ -23,13 +15,5 @@ export const deleteStrike = async (traineeId: string, strikeId: string) => {
 };
 
 export const editStrike = async (traineeId: string, strike: Strike) => {
-  try {
-    await axios.put(`/api/trainees/${traineeId}/strikes/${strike.id}`, strike);
-  } catch (error) {
-    if (axios.isAxiosError(error)) {
-      throw new Error(error.response?.data?.error || 'Failed to edit strike');
-    }
-
-    throw new Error(error instanceof Error ? error.message : 'An unexpected error occurred');
-  }
+  await axios.put(`/api/trainees/${traineeId}/strikes/${strike.id}`, strike);
 };
