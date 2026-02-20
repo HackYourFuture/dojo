c0381109e83f4d351dfe4bc2ee958dd18a152e7e
zalexa19 <zalexa19@gmail.com>
2026-02-20 21:39:18 +0100

    enabled absolute imports on the client

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,4 +1,6 @@
 import { defineConfig, loadEnv } from 'vite';
+
+import path from 'path';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
@@ -7,6 +9,12 @@ export default defineConfig(({ mode }) => {
   const backendProxyTarget = env.VITE_BACKEND_PROXY_TARGET ?? 'http://localhost:7777';
   return {
     plugins: [react()],
+    resolve: {
+      alias: {
+        // This maps the '@' symbol to your 'src' folder
+        '@': path.resolve(__dirname, './src'),
+      },
+    },
     server: {
       // Automatically open the app in the browser on server start.
       open: '/',
