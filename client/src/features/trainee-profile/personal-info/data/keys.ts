b6e424cc265ce6959a6eb7275fcb1a7fa0ab7867
zalexa19 <zalexa19@gmail.com>
2026-02-21 21:17:51 +0100

    typo

--- client/src/features/trainee-profile/personal-info/data/keys.ts
+++ client/src/features/trainee-profile/personal-info/data/keys.ts
@@ -1,4 +1,4 @@
-const TRAINEE_QUERY_KEY = 'traineeIn';
+const TRAINEE_QUERY_KEY = 'traineeInfo';
 
 export const traineeQueryKeys = {
   details: (traineeId: string) => [TRAINEE_QUERY_KEY, 'details', traineeId] as const,
