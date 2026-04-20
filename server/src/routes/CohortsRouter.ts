92b8e60e401ac5672d2160e39ef399a1b8ea0ff4
stasel <2033301+stasel@users.noreply.github.com>
2026-04-21 00:44:45 +0200

    Security update

--- server/src/routes/CohortsRouter.ts
+++ server/src/routes/CohortsRouter.ts
@@ -1,4 +1,5 @@
-import { Router } from 'express';
+import { Request, Response, Router } from 'express';
+import z from 'zod';
 import RouterType from './Router';
 import { CohortsControllerType } from '../controllers/CohortsController';
 import Middleware from '../middlewares/Middleware';
@@ -15,7 +16,29 @@ export class CohortsRouter implements RouterType {
   build(): Router {
     const router = Router();
     this.middlewares.forEach((middleware) => router.use(middleware.handle.bind(middleware)));
-    router.get('/', this.cohortsController.getCohorts.bind(this.cohortsController));
+    router.get('/', this.handleGetCohorts.bind(this));
+
     return router;
   }
+
+  private async handleGetCohorts(req: Request, res: Response) {
+    const QuerySchema = z.object({
+      start: z.coerce.number().int().min(0).default(0),
+      end: z.coerce.number().int().min(0).max(999).default(999),
+    });
+
+    const result = QuerySchema.safeParse(req.query);
+    if (!result.success) {
+      res.status(400).json(z.flattenError(result.error));
+      return;
+    }
+
+    const { start, end } = result.data;
+    const cohorts = await this.cohortsController.getCohorts(start, end);
+    res.status(200).json(cohorts);
+  }
 }
+
+// class CohortsHandler {
+
+// }
